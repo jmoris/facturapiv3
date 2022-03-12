7b771b31dacfabec03f05d7e8d2cef0cf2a3a75f
Jesus Moris <jesus@soluciontotal.cl>
2022-03-12 01:10:05 -0300

    Se agrega migracion de jobs y el sistema de archivos temporal

--- config/filesystems.php
+++ config/filesystems.php
@@ -53,6 +53,12 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
+        'tmp' => [
+            'driver' => 'local',
+            'root' => env('USE_LAMBDA_STORAGE', true) ? "/tmp" : storage_path('app/temp'),
+            'url' => env('USE_LAMBDA_STORAGE', true) ? "/tmp" : storage_path('app/temp'),
+        ],
+
     ],
 
     /*
